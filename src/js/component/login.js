4213321232460d6cee1629571ee48d1a35a68494
Janio Cesar <cesargarcia718@gmail.com>
2020-08-19 22:14:41 +0000

    Worked on education.js, able to see working button on view

--- src/js/component/login.js
+++ src/js/component/login.js
@@ -3,20 +3,21 @@ import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 export const login = () => {
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState(";");
 	return (
 		<>
 			<div className="jumbotron">
 				<h1 className="display-4">Hello, world!</h1>
 				<p className="lead">
-					This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-					featured content or information.
+					This is your login section!!
 				</p>
 				<hr className="my-4" />
 				<p>
 					It uses utility classes for typography and spacing to space content out within the larger container.
 				</p>
 				<a className="btn btn-primary btn-lg" href="#" role="button">
-					Learn more
+					Go home and look at delicious food
 				</a>
 			</div>
 		</>
--- src/js/views/education.js
+++ src/js/views/education.js
@@ -5,33 +5,7 @@ import { Context } from "../store/appContext";
 
 export const Education = props => {
 	const { store, actions } = useContext(Context);
-	useEffect();
-	// () => {
-	// 	if (props.match.params.thetype == "") {
-	// 		actions.getRestaurant(props.match.params.theid);
-	// 	} else if (props.match.params.thetype == "") {
-	// 		actions.getReview(props.match.params.theid);
-	// 	} else {
-	// 		actions.getEducation(props.match.params.theid);
-	// 	}
-	// },
-	// [props.match.params.theid]
-
 	return (
-		/* {"name" in store.single &&
-				props.match.params.theid == store.single.url.match(/[/][0-9]+[/]/)[0].replace(/[/]/g, "") && (
-					<div className="jumbotron">
-						<div className="d-flex">
-							<img className="card-img-top" src="..." alt="Card image cap" />
-						</div>
-						<div className="d-flex">
-							<h1 className="display-4">{store.single.name}</h1>
-							<p className="lead">{props.match.params.thetype}</p>
-						</div>
-						<hr className="my-4" />
-						<p>{"This is the card info"}</p>
-					</div>
-				)} */
 		<Link to="/">
 			<button className="btn btn-primary btn-lg" href="#" role="button">
 				Back home
