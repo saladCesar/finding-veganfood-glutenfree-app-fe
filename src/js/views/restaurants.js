68a56db62235ad9617bc6860ca53e9ba5371574e
kevinfontao <kevinfontao@gmail.com>
2020-09-06 02:11:06 +0000

    worked on restaurant search

--- src/js/views/restaurants.js
+++ src/js/views/restaurants.js
@@ -1,19 +1,49 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import resto1 from "../../img/restaurants/restaurant1.jpg";
+import restowater from "../../img/restaurants/restowater.jpg";
 
 export const Restaurants = props => {
 	const { store, actions } = useContext(Context);
 	return (
-		<>
-			{store.searchResults.map((item, index) => {
-				return <div key={index}>{item.name}</div>;
-			})}
+		<div className="container">
+			<ul className="list-group d-flex flex-row overflow-auto">
+				{store.searchResults.map((item, index) => {
+					return (
+						<div key={index} className="custom-card card">
+							<div
+								className="card"
+								style={{
+									height: "400px",
+									width: "300px"
+								}}>
+								<div
+									className="image1"
+									style={{
+										backgroundImage: `url(${restowater})`,
+										backroundPosition: "center",
+										backgroundSize: "cover",
+										width: "100%",
+										height: "250px"
+									}}
+								/>
+							</div>
+							<div className="card-body">
+								<h5 className="card-title">Name: {item.name}</h5>
+								<p className="card-text">Address: {item.address}</p>
+								<p className="card-text">Diet: {item.diet}</p>
+								<p className="card-text">Phone number: {item.phone_number}</p>
+							</div>
+						</div>
+					);
+				})}
+			</ul>
 			<Link to="/">
 				<button className="btn btn-primary btn-lg" href="#" role="button">
 					Back home
 				</button>
 			</Link>
-		</>
+		</div>
 	);
 };
