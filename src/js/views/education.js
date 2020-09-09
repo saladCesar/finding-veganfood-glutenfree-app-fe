59cb02589cdb36231de35e6dba82d57561f0f769
Janio Cesar <cesargarcia718@gmail.com>
2020-09-09 01:23:32 +0000

    worked on education.js

--- src/js/views/education.js
+++ src/js/views/education.js
@@ -6,23 +6,32 @@ import { Context } from "../store/appContext";
 export const Education = props => {
 	const { store, actions } = useContext(Context);
 	return (
-		<>
-			<div className="container">
-				<h1>Interested in our way of eating?</h1>
-				<div className="row">
-					<div className="style education">
-						<ul className="list-group">
+		<div className="container">
+			<h1>Interested in our way of eating?</h1>
+			<div className="row">
+				<div className="style education col">
+					<ul className="list-group">
+						<div className="col">
 							<li className="list-group-item">Health</li>
 							<p>Healthy habits</p>
+							<script></script>
+						</div>
+						<div className="col">
 							<li className="list-group-item">Nutrition</li>
 							<p>Lets see what the doctors have say!</p>
+						</div>
+						<div className="col">
 							<li className="list-group-item">Ethics</li>
+							<p>We are destroying the planet in the name of food</p>
+						</div>
+						<div className="col">
 							<li className="list-group-item">Documentaries</li>
-						</ul>
-					</div>
+							<p>Earthlings, Forks over Knives, Cowspiracy</p>
+						</div>
+					</ul>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 Education.PropTypes = {
