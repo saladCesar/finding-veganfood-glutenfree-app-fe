575a4ec95bce7a93dc3b352bec3c25cdde86bb37
Janio Cesar <cesargarcia718@gmail.com>
2020-09-10 01:29:10 +0000

    worked on restaurant policy

--- src/js/views/restaurantpolicy.js
+++ src/js/views/restaurantpolicy.js
@@ -7,8 +7,23 @@ export const RestaurantPolicy = props => {
 	const { store, actions } = useContext(Context);
 	return (
 		<>
-			<div className="text">Here we will print the RestaurantPolicy</div>
-
+			<div className="row">
+				<div className="style education col">
+					<ul className="list-group">
+						<div className="col">
+							<h4>
+								{" "}
+								<li className="list-group-item">Restaurant Policy</li>
+							</h4>
+							<h5>Thank you for joining our movement!</h5>
+							<p className="my-2">
+								Please be mindful that after your restaurant is submitted it will undergo processing to
+								determine whether or not your menu meets the diet specific restrictions.
+							</p>
+						</div>
+					</ul>
+				</div>
+			</div>
 			<Link to="/">
 				<button className="btn btn-success btn-lg" href="#" role="button">
 					Back home
